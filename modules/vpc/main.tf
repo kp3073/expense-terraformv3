7c97a7a6cfb0cbe5c97623714f2496a5b3c169b0
kp3073 <keyurpatel8288@gmail.com>
2024-07-01 01:12:13 -0400

    1

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -74,7 +74,7 @@ resource "aws_route_table" "private" {
 
   route {
     cidr_block = "10.0.1.0/24"
-    gateway_id = aws_int_gateway.ngw.id
+    nat_gateway_id = aws_nat_gateway.ngw.id
   }
   tags = {
     Name = "private"
