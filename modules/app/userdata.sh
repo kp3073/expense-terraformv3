69f937bd7df78b0f61447105ff7b0744ebe2411e
kp3073 <keyurpatel8288@gmail.com>
2024-07-03 12:11:53 -0400

    1

--- modules/app/userdata.sh
+++ modules/app/userdata.sh
@@ -2,4 +2,4 @@
 
 dnf install python3.11 -y
 pip3 install boto3 botocore -y
-ansible-pull -i localhost -U https://github.com/kp3073/Expense-ansible main.yml -e role_name=${role_name}
+ansible-pull -i localhost, -U https://github.com/kp3073/Expense-ansible main.yml -e role_name=${role_name}
