ca03f7f487141c301089daeb0bfa3a1b84a29cb3
kp3073 <keyurpatel8288@gmail.com>
2024-07-04 14:03:46 -0400

    1

--- modules/app/userdata.sh
+++ modules/app/userdata.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-dnf install python3.11-pip ansible -y | tee -a /opt/userdata.log
-pip3.11 install boto3 botocore | tee -a /opt/userdata.log
+dnf install ansible python3.11-pip.noarch -y | tee -a /opt/userdata.log
+pip3.11 install botocore boto3 | tee -a /opt/userdata.log
 ansible-pull -i localhost, -U https://github.com/kp3073/Expense-ansible main.yml -e role_name=${role_name} | tee -a /opt/userdata.log
