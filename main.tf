c81310bc16502dfec8c50dcf6efd04f39a2f6eb0
kp3073 <keyurpatel8288@gmail.com>
2024-07-06 00:13:45 -0400

    1

--- main.tf
+++ main.tf
@@ -41,22 +41,22 @@ module "vpc" {
 #   min_size           = var.min_size
 # }
 
-
-module "backend" {
-  depends_on = [module.mysql]
-  source            = "./modules/app"
-  app_port          = 8080
-  component         = "backend"
-  env               = var.env
-  instance_type     = "t3.small"
-  vpc_cidr          = var.vpc_cidr
-  vpc_id            = module.vpc.vpc_id
-  subnets           = module.vpc.private_subnet
-  bastion_node_cidr = var.bastion_node_cidr
-  desired_capacity   = var.desired_capacity
-  max_size           = var.max_size
-  min_size           = var.min_size
-}
+#
+# module "backend" {
+#   depends_on = [module.mysql]
+#   source            = "./modules/app"
+#   app_port          = 8080
+#   component         = "backend"
+#   env               = var.env
+#   instance_type     = "t3.small"
+#   vpc_cidr          = var.vpc_cidr
+#   vpc_id            = module.vpc.vpc_id
+#   subnets           = module.vpc.private_subnet
+#   bastion_node_cidr = var.bastion_node_cidr
+#   desired_capacity   = var.desired_capacity
+#   max_size           = var.max_size
+#   min_size           = var.min_size
+# }
 
 module "mysql" {
   source = "./modules/rds"
