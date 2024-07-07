dbcc4bc22bd36b4549075bcbe11593bbd883c255
kp3073 <keyurpatel8288@gmail.com>
2024-07-07 10:21:39 -0400

    1

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -6,8 +6,8 @@ resource "aws_security_group" "main" {
 
 
   ingress {
-    from_port        = 80
-    to_port          = 80
+    from_port        = 3306
+    to_port          = 3306
     protocol         = "tcp"
     cidr_blocks      = [var.vpc_cidr]
   }
