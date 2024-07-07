c5752ca76bad31af6b7418666481034c9f722e9d
kp3073 <keyurpatel8288@gmail.com>
2024-07-06 21:22:28 -0400

    1

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -102,9 +102,6 @@ resource "aws_launch_template" "temp" {
 }
 resource "aws_autoscaling_group" "asg" {
   name               = "${var.env}-${var.component}"
-  desired_capacity   = var.desired_capacity
-  max_size           = var.max_size
-  min_size           = var.min_size
   vpc_zone_identifier = var.subnets
   target_group_arns = [aws_lb_target_group.tg.arn]
 
