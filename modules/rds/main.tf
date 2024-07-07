dc69737bf012ada34a47792e0389cf1701a354a2
kp3073 <keyurpatel8288@gmail.com>
2024-07-07 10:25:28 -0400

    1

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -37,6 +37,7 @@ resource "aws_rds_cluster" "main" {
   engine                  = "aurora-mysql"
   engine_version          = "5.7.mysql_aurora.2.11.3"
   db_subnet_group_name    = aws_db_subnet_group.main.name
+  vpc_security_group_ids = [aws_security_group.main.id]
   database_name           = "dummy"
   master_username         = data.aws_ssm_parameter.master_username.value
   master_password         = data.aws_ssm_parameter.master_password.value
