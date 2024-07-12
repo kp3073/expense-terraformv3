351251307554b898bfb8c00c07aec0b68e1f3d73
kp3073 <keyurpatel8288@gmail.com>
2024-07-12 12:26:08 -0400

    1

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -50,26 +50,25 @@ resource "aws_route53_record" "main" {
   records = [aws_lb.alb.dns_name]
 }
 
+resource "aws_lb_listener" "listener-http-public" {
+  count             = var.alb_type == "public" ? 1 : 0
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
 
+  default_action {
+    type = "redirect"
 
-# resource "aws_lb_listener" "listener-http-public" {
-#   count             = var.alb_type == "public" ? 1 : 0
-#   load_balancer_arn = aws_lb.alb.arn
-#   port              = "80"
-#   protocol          = "HTTP"
-#
-#   default_action {
-#      type             = "redirect"
-#     redirect {
-#       port = "443"
-#       protocol = "HTTPS"
-#       status_code = "HTTP_301"
-#     }
-#   }
-# }
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
 
 resource "aws_lb_listener" "listener-http-private" {
-#   count             = var.alb_type == "private" ? 1 : 0
+  count             = var.alb_type == "private" ? 1 : 0
   load_balancer_arn = aws_lb.alb.arn
   port              = "80"
   protocol          = "HTTP"
@@ -80,6 +79,7 @@ resource "aws_lb_listener" "listener-http-private" {
   }
 }
 
+
 resource "aws_lb_listener" "listener-https" {
   count             = var.alb_type == "public" ? 1 : 0
   load_balancer_arn = aws_lb.alb.arn
@@ -87,6 +87,7 @@ resource "aws_lb_listener" "listener-https" {
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
   certificate_arn   = "arn:aws:acm:us-east-1:471112727668:certificate/465655e6-3a8f-4aff-af08-99c2764bee2e"
+
 
   default_action {
     type             = "forward"
